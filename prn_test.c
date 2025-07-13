20fad4f2671735a7e7f82b286c00b6cecc1f3416
Yu-Han Liu <C111110117@nkust.edu.tw>
2025-07-13 20:58:01 +0800

    Refine prn_test output

--- prn_test.c
+++ prn_test.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include "bdssim.h"
+
 int main(void){
     sim_config_t c = {0};
     /* Use bundled RINEX file for demonstration */
     snprintf(c.rinex_file, sizeof(c.rinex_file),
              "BRDM00DLR_S_20251760000_01D_MN.rnx");
     init_simulator(&c);
-    puts("PRN01 前 32 chip:");
-    for(int i=0;i<32;i++) printf("%d",prn_code[1][i]);
-    puts(""); return 0;
+
+    for(int prn=1; prn<=63; ++prn){
+        printf("PRN%02d:", prn);
+        for(int i=0;i<16;i++)
+            printf("%d", prn_code[prn][i]);
+        putchar('\n');
+    }
+    cleanup_simulator();
+    return 0;
 }
 
 
