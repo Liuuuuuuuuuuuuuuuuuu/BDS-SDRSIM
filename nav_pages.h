917d499f76edb8ead5d7aedf611d82394be8b4ca
Yu-Han Liu <C111110117@nkust.edu.tw>
2025-07-23 15:12:52 +0800

    refactor: merge nav_pages into navbits

--- nav_pages.h
+++
@@ -1,9 +0,0 @@
-#ifndef NAV_PAGES_H
-#define NAV_PAGES_H
-#include <stdint.h>
-#include "icd_fields.h"
-void assemble_d1_subframe(int sfid, const B1I_D1_Frame *frm,
-                          int week, uint32_t sow, uint32_t words[10]);
-void assemble_d1_subframe2(const B1I_D1_Frame *frm, uint32_t words[10]);
-void assemble_d1_subframe3(const B1I_D1_Frame *frm, uint32_t words[10]);
-#endif
